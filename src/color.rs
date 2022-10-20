659c2127d2874a7fa7ec94dd9afe186bd571e904
MiTiX <bastien.boulet@etu.univ-lyon1.fr>
2022-10-21 00:35:34 +0200

    antialiasing

--- src/color.rs
+++ src/color.rs
@@ -1,10 +1,25 @@
 use crate::vec3::Vec3;
 
-pub fn write_color(color: Vec3) {
+fn clamp(x: f32, min: f32, max: f32) -> f32 {
+    if x < min { return min }
+    if x > max { return max }
+    x
+}
+
+pub fn write_color(color: Vec3, samples_per_pixel: f32) {
+    let mut r: f32 = color.x();
+    let mut g: f32 = color.y();
+    let mut b: f32 = color.z();
+
+    let scale: f32 = 1.0 / samples_per_pixel;
+    r *= scale; 
+    g *= scale;
+    b *= scale;
+
     println!(
         "{} {} {}", 
-        (255.999 * color.x()) as i32,
-        (255.999 * color.y()) as i32,
-        (255.999 * color.z()) as i32
+        (255.999 * clamp(r, 0.0, 0.999)) as i32,
+        (255.999 * clamp(g, 0.0, 0.999)) as i32,
+        (255.999 * clamp(b, 0.0, 0.999)) as i32
     );
 }
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,22 @@ mod color;
 mod ray;
 mod hittable;
 mod sphere;
+mod camera;
 
 use vec3::Vec3;
 use color::write_color;
 use ray::Ray;
 use hittable::{Hittable, HittableList};
 use sphere::Sphere;
+use camera::Camera;
+use rand::prelude::*;
 
 // image
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
 const IMAGE_WIDTH: i32 = 1280;
 const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
 const MAX_VALUE: i32 = 255;
+const SAMPLES_PER_PIXEL: f32 = 100.0;   
 
 // camera
 const VIEWPORT_HEIGHT: f32 = 2.0;
@@ -33,10 +37,8 @@ fn ray_color(ray: &Ray, world: &HittableList) -> Vec3 {
 }
 
 fn main() {
-    let origin = Vec3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+    let camera: Camera = Camera::new(VIEWPORT_HEIGHT, VIEWPORT_WIDTH, FOCAL_LENGTH);
+    let mut rng = rand::thread_rng();
     
     let world = HittableList::new(
         vec![
@@ -55,14 +57,18 @@ fn main() {
     println!("P3\n{} {}\n{}", IMAGE_WIDTH, IMAGE_HEIGHT, MAX_VALUE);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..IMAGE_WIDTH {
-            let u: f32 = i as f32 / (IMAGE_WIDTH-1) as f32;
-            let v: f32 = j as f32 / (IMAGE_HEIGHT-1) as f32;
-            let ray: Ray = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            let pixel_color = ray_color(&ray, &world);
-            
-            write_color(pixel_color);
+            let mut pixel_color = Vec3::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL as i32 {
+                let u: f32 = (i as f32 + rng.gen::<f32>()) / (IMAGE_WIDTH-1) as f32;
+                let v: f32 = (j as f32 + rng.gen::<f32>()) / (IMAGE_HEIGHT-1) as f32;
+                let ray: Ray = camera.get_ray(u, v);
+
+                pixel_color = pixel_color + ray_color(&ray, &world);
+            }
+            write_color(pixel_color, SAMPLES_PER_PIXEL);
         }
     }
     eprintln!("Done");
